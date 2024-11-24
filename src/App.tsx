4052613ac93cbed9e4465c40ac9921bfa10ff4aa
jacques zhu <jacques.zhu19@gmail.com>
2024-11-24 15:35:34 +0100

    feat: Added specific URLs for in-progress tasks (/tasks/in-progress) and completed tasks (/tasks/completed)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'    
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import TaskList from './components/TaskList'
+import './App.css'
 
 function App() {
   return (
-    <div className="app-container">
-      <header className="header">
-        <h1>Gestionnaire de Tâches</h1>
-      </header>
-      <main className="main-content">
-        <TaskList />
-      </main>
-    </div>
+    <Router>
+      <div className="app-container">
+        <header className="header">
+          <h1>Gestionnaire de Tâches</h1>
+        </header>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<TaskList />} />
+            <Route path="/taches/en-cours" element={<TaskList showPendingOnly={true} />} />
+            <Route path="/taches/terminees" element={<TaskList showCompletedOnly={true} />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
