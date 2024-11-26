24e80f675396d941c93bc315fae80fea59dab484
jacques zhu <jacques.zhu19@gmail.com>
2024-11-26 14:37:43 +0100

    bugfix ThemeController

--- src/controllers/ThemeController.ts
+++ src/controllers/ThemeController.ts
@@ -1,25 +1,33 @@
-import { ThemeModel } from '../models/ThemeModel'
-import { ThemeColor } from '../types/theme'
+import { ThemeColor, ThemeColors } from '../types/theme'
+import { LocalStorageService } from '../services/LocalStorageService'
+import { ThemeService } from '../services/ThemeService'
 
 export class ThemeController {
-  private model: ThemeModel
+  private currentTheme: ThemeColor
   private notifyChange: () => void
 
   constructor(notifyChange: () => void) {
-    this.model = new ThemeModel()
+    this.currentTheme = LocalStorageService.getTheme()
     this.notifyChange = notifyChange
+    this.applyTheme(this.currentTheme)
   }
 
   getTheme(): ThemeColor {
-    return this.model.getTheme()
+    return this.currentTheme
   }
 
   setTheme(color: ThemeColor): void {
-    this.model.setTheme(color)
+    this.currentTheme = color
+    LocalStorageService.setTheme(color)
+    this.applyTheme(color)
     this.notifyChange()
   }
 
-  getColors() {
-    return this.model.getColors()
+  getColors(): Record<ThemeColor, ThemeColors> {
+    return ThemeService.getDefaultColors()
+  }
+
+  private applyTheme(theme: ThemeColor): void {
+    ThemeService.applyTheme(theme, this.getColors())
   }
 } 
